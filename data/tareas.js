ac1b11701f1ea0efac2cea9636fbd509241219c8
Mauricio Rodrigo Rodriguez Palacios <mauricio611rp@gmail.com>
2024-10-23 16:54:32 -0400

    Configuracion de la API y creacion de la ruta para agregar una tarea

--- data/tareas.js
+++ data/tareas.js
@@ -3,5 +3,20 @@
     "id": 1,
     "descripcion": "sadasd",
     "realizada": false
+  },
+  {
+    "id": 2,
+    "descripcion": "Tarea 1",
+    "realizada": false
+  },
+  {
+    "id": 3,
+    "descripcion": "Tarea 25",
+    "realizada": false
+  },
+  {
+    "id": 4,
+    "descripcion": "Tarea 1",
+    "realizada": false
   }
 ]
