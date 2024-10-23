a4c067fd95dc4f3c212997dce4b89bccda0129e9
Mauricio Rodrigo Rodriguez Palacios <mauricio611rp@gmail.com>
2024-10-23 19:57:33 -0400

    Rutas para obtener una tarea y todas las tareas

--- routes/tareasRoutes.js
+++ routes/tareasRoutes.js
@@ -8,7 +8,7 @@ const listaController = new ListaController();
 router.post("/tareas", (req, res) => {
   try {
     listaController.agregarTarea(req.body.descripcion);
-    res.status(200).send("Tarea agregada exitosamente");
+    res.status(200).send({ mensaje: "Tarea agregada exitosamente" });
   } catch (error) {
     return res.status(500).send({ error: "Ingrese una descripción valida" });
   }
@@ -17,7 +17,7 @@ router.post("/tareas", (req, res) => {
 router.delete("/tareas/:id", (req, res) => {
   try {
     listaController.eliminarTarea(parseInt(req.params.id));
-    res.status(200).send("Tarea eliminada exitosamente.");
+    res.status(200).send({ mensaje: "Tarea eliminada exitosamente." });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
@@ -38,6 +38,25 @@ router.put("/tareas/:id", (req, res) => {
   }
 });
 
+router.get("/tareas", (req, res) => {
+  try {
+    const tareasArchivo = listaController.obtenerTareas();
+    let tareas = [];
+    tareasArchivo.forEach((tarea) => {
+      tareas.push({
+        ID: tarea.getId(),
+        Descripcion: tarea.getDescripcion(),
+        Estado: tarea.tareaRealizada() ? "Realizada" : "Pendiente",
+      });
+    });
+    res.status(200).send(tareas);
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ error: "Ocurrió un error al obtener las tareas." });
+  }
+});
+
 router.get("/tareas_pendientes", (req, res) => {
   try {
     const tareasPendientes = listaController.tareasPendientes();
@@ -54,6 +73,23 @@ router.get("/tareas_pendientes", (req, res) => {
     return res
       .status(500)
       .send({ error: "Ocurrió un error al obtener las tareas." });
+  }
+});
+
+router.get("/tareas/:id", (req, res) => {
+  try {
+    const tarea = listaController.obtenerTareaPorId(parseInt(req.params.id));
+    res.status(200).send({
+      ID: tarea.getId(),
+      Descripcion: tarea.getDescripcion(),
+      Estado: tarea.tareaRealizada() ? "Realizada" : "Pendiente",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      error:
+        "Ocurrio un error, verifique que exista una tarea con el ID proporcionado.",
+    });
   }
 });
 
